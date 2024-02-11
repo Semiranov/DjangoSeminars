a288473d1b1c4896d84ac5c38319316505619d03
Semiranov <semiranov@gmail.com>
2024-02-12 00:01:32 +0300

    1

---
+++ app_2/migrations/0001_initial.py
@@ -0,0 +1,46 @@
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Client',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=70)),
+                ('email', models.EmailField(max_length=254)),
+                ('phone', models.CharField(max_length=70)),
+                ('address', models.CharField(max_length=200)),
+                ('register_date', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=100)),
+                ('description', models.TextField()),
+                ('price', models.DecimalField(decimal_places=2, max_digits=10)),
+                ('count', models.IntegerField()),
+                ('add_date', models.DateTimeField(auto_now_add=True)),
+                ('image_product', models.ImageField(upload_to='images/')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Order',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('order_sum', models.DecimalField(decimal_places=2, max_digits=10)),
+                ('order_date', models.DateField(auto_now_add=True)),
+                ('client', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='app_2.client')),
+                ('product', models.ManyToManyField(to='app_2.product')),
+            ],
+        ),
+    ]
